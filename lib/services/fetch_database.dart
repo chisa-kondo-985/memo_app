119a5f080a24a5d64f0cf9e5cedbdb3fb74b7e8f
chisa-kondo-985 <chisa.kondo@psp.co.jp>
2024-08-06 18:44:55 +0900

    2024-08-06
    ・いらないコードの削除

--- lib/services/fetch_database.dart
+++ lib/services/fetch_database.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:memo_app/model/response_result.dart';
 import 'package:memo_app/model/item_model.dart';
